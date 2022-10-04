f3f96af8e29e5f617a3c6cc6b446b4d744c34029
yongjiezhao <yongjiezhao@tencent.com>
2022-10-04 21:28:25 +0800

    btree

--- main.go
+++ main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{}
-
-	fmt.Println(nums[:0])
+	ret := [][]int{}
+	nums := make([]int, 0, 4)
+	nums = append(nums, 1)
+	nums = append(nums, 2)
+	nums = append(nums, 3)
+	ret = append(ret, nums)
+	fmt.Println(ret)
+	nums = append(nums, 4)
+	fmt.Println(ret)
 }
