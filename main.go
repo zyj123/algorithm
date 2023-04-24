e01baa8107f0f3d6e3ad876139629a158db060cd
yongjiezhao <yongjiezhao@tencent.com>
2023-04-24 20:16:44 +0800

    edit

--- main.go
+++ main.go
@@ -1,32 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
+	//chars1 := [][]byte{
+	//	{'1', ' ', '3'},
+	//	{'-', '+', '-'},
+	//	{'2', ' ', '4'},
+	//}
+	//chars2 := [][]byte{
+	//	{' ', ' ', '2', ' ', ' '},
+	//	{'1', '+', '-', '/', '2'},
+	//	{' ', ' ', '3', ' ', ' '},
+	//}
+
 	// read from file
 	// 假设字符串是有效的表达式
-	chars := [][]byte{
-		{'1', ' ', '3'},
-		{'-', '+', '-'},
-		{'2', ' ', '4'},
+	chars1 := readFile("./example1.txt")
+	chars2 := readFile("./example2.txt")
+	fmt.Println(chars1, chars2)
+
+	fmt.Println(calculator(chars1) == "5/4")
+	fmt.Println(calculator(chars2) == "4/3")
+}
+
+func readFile(file string) [][]byte {
+	f, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	res := make([][]byte, 0)
+	for scanner.Scan() {
+		res = append(res, scanner.Bytes())
+	}
+	if err = scanner.Err(); err != nil {
+		panic(err)
+	}
+	if len(res) != 3 || len(res[0]) != len(res[2]) {
+		panic(fmt.Errorf("file is illegal"))
 	}
 
-	calculator(chars)
+	return res
 }
 
 func calculator(chars [][]byte) string {
 	stack := make([][]int, 0)
 	var preSign byte = '+'
-	n := len(chars[0])
-
-	for i := 0; i < n; i++ {
-		var (
-			line1 = chars[0]
-			line2 = chars[1]
-			line3 = chars[2]
-		)
+	var (
+		line1 = chars[0]
+		line2 = chars[1]
+		line3 = chars[2]
+	)
+	for i := 0; i < len(line2); i++ {
 		// 符号位
-		if line1[i] == ' ' && !isDigit(line2[i]) {
+		if (i >= len(line1) && !isDigit(line2[i])) || (i < len(line1) && line1[i] == ' ' && !isDigit(line2[i])) {
 			preSign = line2[i]
 			continue
 		}
@@ -54,10 +88,8 @@ func calculator(chars [][]byte) string {
 	for _, v := range stack {
 		res = add(res, v)
 	}
-	// 计算分数的值
-	fmt.Println(res)
-
-	return ""
+	res = simplify(res)
+	return fmt.Sprintf("%d/%d", res[0], res[1])
 }
 
 func toInt(i byte) int {
@@ -70,30 +102,32 @@ func isDigit(c byte) bool {
 
 // 分数计算 []int{分子，分母}
 func add(a, b []int) []int {
-	denominatorLcm := lcm(a[1], b[1])
-	numeratorA := a[0] * (denominatorLcm / a[1])
-	numeratorB := b[0] * (denominatorLcm / b[1])
-	return []int{numeratorA + numeratorB, denominatorLcm}
+	denominator := a[1] * b[1]
+	numeratorA := a[0] * b[1]
+	numeratorB := b[0] * a[1]
+	return simplify([]int{numeratorA + numeratorB, denominator})
 }
 func sub(a, b []int) []int {
-	denominatorLcm := lcm(a[1], b[1])
-	numeratorA := a[0] * (denominatorLcm / a[1])
-	numeratorB := b[0] * (denominatorLcm / b[1])
-	return []int{numeratorA - numeratorB, denominatorLcm}
+	denominator := a[1] * b[1]
+	numeratorA := a[0] * b[1]
+	numeratorB := b[0] * a[1]
+	return simplify([]int{numeratorA - numeratorB, denominator})
 }
 func multi(a, b []int) []int {
 	numerator := a[0] * b[0]
 	denominator := a[1] * b[1]
-	return []int{numerator, denominator}
+	return simplify([]int{numerator, denominator})
 }
 func divide(a, b []int) []int {
 	numerator := a[0] * b[1]
 	denominator := a[1] * b[0]
-	return []int{numerator, denominator}
+	return simplify([]int{numerator, denominator})
 }
 
-func lcm(a, b int) int {
-	return a * b / gcd(a, b)
+// 化简分数
+func simplify(a []int) []int {
+	g := gcd(a[0], a[1])
+	return []int{a[0] / g, a[1] / g}
 }
 
 func gcd(a, b int) int {
